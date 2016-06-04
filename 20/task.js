fec4a9a3609b079b317e54603ef49fb4d409944f
228806684@qq.com <228806684@qq.com>
2016-06-04 22:39:53 +0800

    更新任务20和readme

--- 20/task.js
+++ 20/task.js
@@ -1,141 +1,109 @@
-window.onload = function () {
+/**
+ *  队列类
+ */
 
-// 跨浏览器事件处理程序
-var eventUtil = {
-  // 添加句柄
-  addHandler : function (element, type, handler) {
-    if (element.addEventListener) {
-      element.addEventListener(type, handler, false);
-    }
-    else if (element.attachEvent) {
-      element.attachEvent('on' + type, handler);
-    }
-    else {
-      element['on' + type] = handler;
-    }
+function Queue() {
+  // 用于存储队列元素
+  this.queue = [];
+  // 渲染队列区域
+  this.queueWrap = document.getElementById("queue-wrap");
+  // 操作区域
+  this.box = document.getElementById("box");
+  // 查询按钮
+  this.queryBtn = document.getElementById("querybtn");
+}
+
+Queue.prototype = {
+
+  // 对象初始化
+  init: function() {
+    this.initOpButton();
+    this.initNumBlock();
   },
-  // 删除句柄
-  removeHandler : function (element, type, handler) {
-    if (element.removeEventListener) {
-      element.removeEventListener(type, handler, false);
-    }
-    else if (element.detachEvent) {
-      element.detachEvent('on' + type, handler);
-    }
-    else {
-      element['on' + type] = null;
-    }
-  }
-}
 
-// 用于存储队列的数据
-var Queue = [];
+  initOpButton: function() {
+    var me = this;
 
-/**
- * 初始化队列数字
- */
-function initNumBlock() {
-
-  var queueWarp = document.getElementById("queue-wrap");
-
-  eventUtil.addHandler(queueWarp, "click", function(e){
-    if(e.target.className.toLowerCase() === 'block'){
-      var index = e.target.getAttribute('data-index');
-      Queue.splice(index,1);
-      renderQueue();
-    }
-  });
-}
-
-/**
- * 点击按钮事件的处理函数
- */
-function buttonHandle(obj) {
-
-  // 获取输入值
-  var content = document.getElementById("content").value.trim();
-  
-  switch(obj.target.id)
-  {
-    case "leftin":
-      if (content) {
-        var strArr = content.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).forEach(function(item,index){
-          Queue.unshift(item);
-        });
-      } else {
-        alert("请输入内容");
+    eventUtil.addHandler(me.box, "click", function(event){
+      if(event.target.className.toLowerCase() === 'opreate') {
+        me.opButtonHandle(event);
       }
-      break;
-    case "rightin": 
-      if (content) {
-        var strArr = content.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).forEach(function(item,index){
-          Queue.push(item);
-        });
-      } else {
-        alert("请输入内容");
-      }
-      break;
-    case "leftout":
-      if (Queue.length > 0) {
-        alert(Queue.shift());
-      } else {
-        alert("当前队列为空！");
-      }
-      break;
-    case "rightout":
-      if (Queue.length > 0) {
-        alert(Queue.pop());
-      } else {
-        alert("当前队列为空！");
-      }
-      break;
-  }
-  renderQueue();
-  
-}
+    });
 
-/**
- * 初始化队列操作按钮事件
- */
-function initOpButton() {
+    eventUtil.addHandler(me.queryBtn, "click", me.renderQueue);
+  },
 
-  var box = document.getElementById("box");
-  var queryBtn = document.getElementById("querybtn");
-
-  eventUtil.addHandler(box, "click", function(event){
-    if(event.target.className.toLowerCase() === 'opreate') {
-      buttonHandle(event);
+  // 队列操作方法
+  opButtonHandle: function(obj) {
+    var me = this;
+    // 获取输入值
+    var content = document.getElementById("content").value.trim();
+    var strArr = "";
+    switch(obj.target.id)
+    {
+      case "leftin":
+        if (content) {
+          strArr = content.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).forEach(function(item,index){
+            me.queue.unshift(item);
+            console.log(me.queue);
+          });
+        } else {
+          alert("请输入内容");
+        }
+        break;
+      case "rightin": 
+        if (content) {
+          strArr = content.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).forEach(function(item,index){
+            me.queue.push(item);
+          });
+        } else {
+          alert("请输入内容");
+        }
+        break;
+      case "leftout":
+        if (me.queue.length > 0) {
+          alert(me.queue.shift());
+        } else {
+          alert("当前队列为空！");
+        }
+        break;
+      case "rightout":
+        if (me.queue.length > 0) {
+          alert(me.queue.pop());
+        } else {
+          alert("当前队列为空！");
+        }
+        break;
     }
-  });
+    this.renderQueue();
+  },
 
-  eventUtil.addHandler(queryBtn, "click", renderQueue);
+  // 初始化队列块方法
+  initNumBlock: function() {
+    var me = this;
+    eventUtil.addHandler(me.queueWrap, "click", function(e){
+      if(e.target.className.toLowerCase() === 'block'){
+        var index = e.target.getAttribute('data-index');
+        me.queue.splice(index,1);
+        me.renderQueue();
+      }
+    });
 
-}
+  },
 
-/**
- * 渲染队列
- */
-function renderQueue() {
+  // 渲染列表
+  renderQueue: function() {
 
-  var queueWrap = document.getElementById("queue-wrap");
-  var queueHtml = "";
-  var keyword = document.getElementById("keyword").value.trim();
+    var queueHtml = "";
+    var keyword = document.getElementById("keyword").value.trim();
 
-  for (var i = 0; i < Queue.length; i++) {
-    queueHtml += "<div class='block' data-index='"+ i +"'>"+ Queue[i].replace(new RegExp(keyword, "g"),"<span style='color:blue'>" + keyword + "</span>") +"</div>";
+    for (var i = 0; i < queue.queue.length; i++) {
+      queueHtml += "<div class='block' data-index='"+ i +"'>"+ queue.queue[i].replace(new RegExp(keyword, "g"),"<span style='color:blue'>" + keyword + "</span>") +"</div>";
+    }
+
+    queue.queueWrap.innerHTML = queueHtml;
   }
 
-  queueWrap.innerHTML = queueHtml;
-}
+};
 
-/**
- * 初始化函数
- */
-function init() {
-  initOpButton();
-  initNumBlock();
-}
 
-init();
-
-}
